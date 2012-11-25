3b09e3d7d975522dfa3a99acf194a5399e377301
Jürgen Brandstetter <juergen@tagaprice.org>
2012-11-25 16:08:52 +0100

    first bt example works now

--- android/gen/at/ac/tuwien/sportmate/R.java
+++ android/gen/at/ac/tuwien/sportmate/R.java
@@ -11,23 +11,31 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int btn_square_overlay_normal=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int progress_horizontal=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070002;
-        public static final int pager=0x7f070000;
-        public static final int pager_title_strip=0x7f070001;
+        public static final int ColorIndicator=0x7f070000;
+        public static final int button1=0x7f070001;
+        public static final int first_button=0x7f070002;
+        public static final int menu_settings=0x7f070003;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
+        public static final int bttestapp=0x7f030001;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int blue=0x7f040008;
+        public static final int green=0x7f040007;
         public static final int hello_world=0x7f040004;
         public static final int menu_settings=0x7f040005;
+        public static final int red=0x7f040006;
         public static final int title_section1=0x7f040003;
         public static final int title_section2=0x7f040002;
         public static final int title_section3=0x7f040001;
--- android/src/at/ac/tuwien/sportmate/MainActivity.java
+++ android/src/at/ac/tuwien/sportmate/MainActivity.java
@@ -1,127 +1,81 @@
 package at.ac.tuwien.sportmate;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
-import android.support.v4.app.NavUtils;
-import android.support.v4.view.ViewPager;
-import android.view.Gravity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import at.abraxas.amarino.Amarino;
 
-public class MainActivity extends FragmentActivity {
-
-	/**
-	 * The {@link android.support.v4.view.PagerAdapter} that will provide
-	 * fragments for each of the sections. We use a
-	 * {@link android.support.v4.app.FragmentPagerAdapter} derivative, which
-	 * will keep every loaded fragment in memory. If this becomes too memory
-	 * intensive, it may be best to switch to a
-	 * {@link android.support.v4.app.FragmentStatePagerAdapter}.
+public class MainActivity extends Activity{
+	
+	private static final String TAG = "MultiColorLamp";
+	
+	/* TODO: change the address to the address of your Bluetooth module
+	 * and ensure your device is added to Amarino
 	 */
-	SectionsPagerAdapter mSectionsPagerAdapter;
+	private static final String DEVICE_ADDRESS = "00:06:66:49:30:1E";
+	
+	final int DELAY = 150;
+	Button mybutton;
+	View colorIndicator;
+	
+	int nine = 0;
+	long lastChange;
 
-	/**
-	 * The {@link ViewPager} that will host the section contents.
-	 */
-	ViewPager mViewPager;
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
-
-		// Create the adapter that will return a fragment for each of the three
-		// primary sections of the app.
-		mSectionsPagerAdapter = new SectionsPagerAdapter(
-				getSupportFragmentManager());
-
-		// Set up the ViewPager with the sections adapter.
-		mViewPager = (ViewPager) findViewById(R.id.pager);
-		mViewPager.setAdapter(mSectionsPagerAdapter);
-
-	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.activity_main, menu);
-		return true;
-	}
-
-	/**
-	 * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
-	 * one of the sections/tabs/pages.
-	 */
-	public class SectionsPagerAdapter extends FragmentPagerAdapter {
-
-		public SectionsPagerAdapter(FragmentManager fm) {
-			super(fm);
-		}
-
-		@Override
-		public Fragment getItem(int position) {
-			// getItem is called to instantiate the fragment for the given page.
-			// Return a DummySectionFragment (defined as a static inner class
-			// below) with the page number as its lone argument.
-			Fragment fragment = new DummySectionFragment();
-			Bundle args = new Bundle();
-			args.putInt(DummySectionFragment.ARG_SECTION_NUMBER, position + 1);
-			fragment.setArguments(args);
-			return fragment;
-		}
-
-		@Override
-		public int getCount() {
-			// Show 3 total pages.
-			return 3;
-		}
-
-		@Override
-		public CharSequence getPageTitle(int position) {
-			switch (position) {
-			case 0:
-				return getString(R.string.title_section1).toUpperCase();
-			case 1:
-				return getString(R.string.title_section2).toUpperCase();
-			case 2:
-				return getString(R.string.title_section3).toUpperCase();
+		Amarino.connect(this, DEVICE_ADDRESS);
+        
+        // get references to views defined in our main.xml layout file
+		mybutton = (Button) findViewById(R.id.button1);
+		
+		mybutton.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Log.d("p", "press button");
+				
+				 updateLed();
+				
 			}
-			return null;
-		}
+		});
+		
+        colorIndicator = findViewById(R.id.ColorIndicator);
+
+    }
+    
+	@Override
+	protected void onStart() {
+		super.onStart();
+
+        
+        // set seekbars and feedback color according to last state
+        
 	}
 
-	/**
-	 * A dummy fragment representing a section of the app, but that simply
-	 * displays dummy text.
-	 */
-	public static class DummySectionFragment extends Fragment {
-		/**
-		 * The fragment argument representing the section number for this
-		 * fragment.
-		 */
-		public static final String ARG_SECTION_NUMBER = "section_number";
-
-		public DummySectionFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			// Create a new TextView and set its text to the fragment's section
-			// number argument value.
-			TextView textView = new TextView(getActivity());
-			textView.setGravity(Gravity.CENTER);
-			textView.setText(Integer.toString(getArguments().getInt(
-					ARG_SECTION_NUMBER)));
-			return textView;
-		}
+	@Override
+	protected void onStop() {
+		super.onStop();
+		// save state
+		
+		// stop Amarino's background service, we don't need it any more 
+		Amarino.disconnect(this, DEVICE_ADDRESS);
 	}
 
+
+	private void updateLed(){
+		Amarino.sendDataToArduino(this, DEVICE_ADDRESS, 'A', 0);
+	}
+
+	
 }
